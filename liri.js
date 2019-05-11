59af056f04869702ee3800523d24da6975a99bdc
Spinning Plates <alec.munguia@gmail.com>
2019-05-11 18:43:41 -0500

    Required all the things

--- liri.js
+++ liri.js
@@ -1,6 +1,29 @@
+// NOTES //
+// API KEYS
+// AXIOS
+// VARIABLES FOR COMMAND LINE ARGUEMENTS
+// 
+// A SWITCH STATEMENT TO CHANGE BETWEEN INPUT TYPES
+// FUNCTIONS TO RUN EACH API REQUEST
+
+// npm install dotenv
+// npm install --save node-spotify-api
+// npm install axios
+
+
+
+//require all the things
 require("dotenv").config();
 
-var keys = require("./keys.js");
+var fs = require('fs');
 
+var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+
+var moment = require("moment");
+
+var axios = require("axios");
+
+
 
